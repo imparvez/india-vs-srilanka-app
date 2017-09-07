483f7346dcf94fd5678035f48e786d966296ac82
Parvez Shaikh <parvez.shaikh@bookmyshow.com>
2017-09-07 18:16:02 +0530

    Update the static data

--- app/App.js
+++ app/App.js
@@ -7,19 +7,35 @@ export default class App extends Component {
 			{
 				'date' : '14 Feb 2016',
 				'result': 'India won by 9 wickets',
-				'synopsis': 'Lorem Ipsum Lorem ipsum Lorem Ipsum. Lorem Ipsum Lorem ipsum Lorem Ipsum. Lorem Ipsum Lorem ipsum Lorem Ipsum. Lorem Ipsum Lorem ipsum Lorem Ipsum. Lorem Ipsum Lorem ipsum Lorem Ipsum'
+				'synopsis': 'Lorem Ipsum Lorem ipsum Lorem Ipsum. Lorem Ipsum Lorem ipsum Lorem Ipsum. Lorem Ipsum Lorem ipsum Lorem Ipsum. Lorem Ipsum Lorem ipsum Lorem Ipsum. Lorem Ipsum Lorem ipsum Lorem Ipsum',
+				'teamAName': 'Sri Lanka',
+				'teamAScore': '82/10 (18)',
+				'teamBName': 'India',
+				'teamBScore': '84/1 (13.5)'
 			}, {
 				'date' : '1 Mar 2016',
 				'result': 'India won by 5 wickets',
-				'synopsis': 'Lorem Ipsum Lorem ipsum Lorem Ipsum. Lorem Ipsum Lorem ipsum Lorem Ipsum. Lorem Ipsum Lorem ipsum Lorem Ipsum. Lorem Ipsum Lorem ipsum Lorem Ipsum. Lorem Ipsum Lorem ipsum Lorem Ipsum'
+				'synopsis': 'Lorem Ipsum Lorem ipsum Lorem Ipsum. Lorem Ipsum Lorem ipsum Lorem Ipsum. Lorem Ipsum Lorem ipsum Lorem Ipsum. Lorem Ipsum Lorem ipsum Lorem Ipsum. Lorem Ipsum Lorem ipsum Lorem Ipsum',
+				'teamAName': 'Sri Lanka',
+				'teamAScore': '82/10 (18)',
+				'teamBName': 'India',
+				'teamBScore': '84/1 (13.5)'
 			}, {
 				'date' : 'Thu, 8 Jun',
 				'result': 'Sri Lanka won by 7 wickets',
-				'synopsis': 'Lorem Ipsum Lorem ipsum Lorem Ipsum. Lorem Ipsum Lorem ipsum Lorem Ipsum. Lorem Ipsum Lorem ipsum Lorem Ipsum. Lorem Ipsum Lorem ipsum Lorem Ipsum. Lorem Ipsum Lorem ipsum Lorem Ipsum'
+				'synopsis': 'Lorem Ipsum Lorem ipsum Lorem Ipsum. Lorem Ipsum Lorem ipsum Lorem Ipsum. Lorem Ipsum Lorem ipsum Lorem Ipsum. Lorem Ipsum Lorem ipsum Lorem Ipsum. Lorem Ipsum Lorem ipsum Lorem Ipsum',
+				'teamAName': 'Sri Lanka',
+				'teamAScore': '82/10 (18)',
+				'teamBName': 'India',
+				'teamBScore': '84/1 (13.5)'
 			}, {
 				'date' : 'Wed, 26 Jul',
 				'result': 'India won by 304 runs',
-				'synopsis': 'Lorem Ipsum Lorem ipsum Lorem Ipsum. Lorem Ipsum Lorem ipsum Lorem Ipsum. Lorem Ipsum Lorem ipsum Lorem Ipsum. Lorem Ipsum Lorem ipsum Lorem Ipsum. Lorem Ipsum Lorem ipsum Lorem Ipsum'
+				'synopsis': 'Lorem Ipsum Lorem ipsum Lorem Ipsum. Lorem Ipsum Lorem ipsum Lorem Ipsum. Lorem Ipsum Lorem ipsum Lorem Ipsum. Lorem Ipsum Lorem ipsum Lorem Ipsum. Lorem Ipsum Lorem ipsum Lorem Ipsum',
+				'teamAName': 'Sri Lanka',
+				'teamAScore': '82/10 (18)',
+				'teamBName': 'India',
+				'teamBScore': '84/1 (13.5)'
 			}
 		]
 		return(
