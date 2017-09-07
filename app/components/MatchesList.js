b7d6bf0e5ccbb6ba9140de0e9c965c6605747734
Parvez Shaikh <parvez.shaikh@bookmyshow.com>
2017-09-07 18:17:40 +0530

    Update the matchList component

--- app/components/MatchesList.js
+++ app/components/MatchesList.js
@@ -4,30 +4,49 @@ import Collapsible from 'react-collapsible'
 export default class MatchesList extends Component {
     constructor(props){
         super(props);
-        
-        this.state = {
-            open: false,
-            class: 'section'
-        }
     }
 
     render(){
-        let handleClickEvent = this.handleClick;
-        let sectionClass = this.state.class;
+    	const teamASmallImage = 'https://ssl.gstatic.com/onebox/media/sports/logos/-6moqr0dxYpIhERhbiVLSA_56x42.png';
+    	const teamBSmallImage = 'https://ssl.gstatic.com/onebox/media/sports/logos/mlXOOB9HXxLpoeS2dHSgGA_56x42.png';
+
+    	const teamALargeImage = 'https://ssl.gstatic.com/onebox/media/sports/logos/-6moqr0dxYpIhERhbiVLSA_96x72.png';
+    	const teamBLargeImage = 'https://ssl.gstatic.com/onebox/media/sports/logos/mlXOOB9HXxLpoeS2dHSgGA_96x72.png';
+
         const matchListItem = this.props.data.map(function(item, index){
             return (
                 <li className="list-group-item" key={index}>
-                    <span className="date-container">{item.date}</span>
-                    <span className="sriLankaImageContainer">
-                        <img src="https://ssl.gstatic.com/onebox/media/sports/logos/-6moqr0dxYpIhERhbiVLSA_56x42.png" alt="Sri Lanka"/>
-                    </span>
-                    <span className="indiaImageContainer">
-                        <img src="https://ssl.gstatic.com/onebox/media/sports/logos/mlXOOB9HXxLpoeS2dHSgGA_56x42.png" alt="India"/>
-                    </span>
-                    <span className="result-container">{item.result}</span>
-                    <Collapsible trigger="Start here">
-                        <div className={`view-more ${sectionClass}`} >
-                            <p className="synopsis-container">{item.synopsis}</p>
+                    <span className="date-container col-md-4 col-sm-12 col-lg-4">{item.date}</span>
+
+                    <div className="image-container col-md-4 col-sm-12 col-lg-4">
+	                    <span className="sriLankaImageContainer">
+	                        <img src={teamASmallImage} alt={item.teamAName}/>
+	                    </span>
+	                    <span className="indiaImageContainer">
+	                        <img src={teamBSmallImage} alt={item.teamBName}/>
+	                    </span>
+                    </div>
+
+                    <span className="result-container col-md-4 col-sm-12 col-lg-4">{item.result}</span>
+                    
+                    <Collapsible trigger="Schedule & Scores" className="">
+                        <div className={`view-more`} >
+                            <p className="synopsis-container">
+                            	<div className="teamAScoreContainer col-sm-4">
+                            		<span className="teamAName">{item.teamAName}</span>
+                            		<span className="teamAImageContainer">
+                            			<img src={teamALargeImage} alt={item.teamAName}/>
+                            		</span>
+                            	</div>
+                            	<span className="col-sm-4">vs</span>
+                            	<div className="teamBScoreContainer col-sm-4">
+                            		<span className="teamBName">{item.teamBName}</span>
+                            		<span className="teamBImageContainer">
+                            			<img src={teamBLargeImage} alt={item.teamBName}/>
+                            		</span>
+                            	</div>
+                            	<span className="result">{item.result}</span>
+                            </p>
                         </div>
                     </Collapsible>
                 </li>
