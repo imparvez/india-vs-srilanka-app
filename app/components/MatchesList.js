e46398bef8d99e018d2c3f3de6c4e263658f6e99
Parvez Shaikh <parvezshaikhj@gmail.com>
2017-09-10 20:23:30 +0530

    UI update

--- app/components/MatchesList.js
+++ app/components/MatchesList.js
@@ -7,11 +7,11 @@ export default class MatchesList extends Component {
     }
 
     render(){
-    	const teamASmallImage = 'https://ssl.gstatic.com/onebox/media/sports/logos/-6moqr0dxYpIhERhbiVLSA_56x42.png';
-    	const teamBSmallImage = 'https://ssl.gstatic.com/onebox/media/sports/logos/mlXOOB9HXxLpoeS2dHSgGA_56x42.png';
+        const teamASmallImage = 'https://ssl.gstatic.com/onebox/media/sports/logos/-6moqr0dxYpIhERhbiVLSA_56x42.png';
+        const teamBSmallImage = 'https://ssl.gstatic.com/onebox/media/sports/logos/mlXOOB9HXxLpoeS2dHSgGA_56x42.png';
 
-    	const teamALargeImage = 'https://ssl.gstatic.com/onebox/media/sports/logos/-6moqr0dxYpIhERhbiVLSA_96x72.png';
-    	const teamBLargeImage = 'https://ssl.gstatic.com/onebox/media/sports/logos/mlXOOB9HXxLpoeS2dHSgGA_96x72.png';
+        const teamALargeImage = 'https://ssl.gstatic.com/onebox/media/sports/logos/-6moqr0dxYpIhERhbiVLSA_96x72.png';
+        const teamBLargeImage = 'https://ssl.gstatic.com/onebox/media/sports/logos/mlXOOB9HXxLpoeS2dHSgGA_96x72.png';
 
         const matchListItem = this.props.data.map(function(item, index){
             return (
@@ -19,12 +19,12 @@ export default class MatchesList extends Component {
                     <span className="date-container col-md-4 col-sm-12 col-lg-4">{item.date}</span>
 
                     <div className="image-container col-md-4 col-sm-12 col-lg-4">
-	                    <span className="sriLankaImageContainer">
-	                        <img src={teamASmallImage} alt={item.teamAName}/>
-	                    </span>
-	                    <span className="indiaImageContainer">
-	                        <img src={teamBSmallImage} alt={item.teamBName}/>
-	                    </span>
+                        <span className="sriLankaImageContainer">
+                            <img src={teamASmallImage} alt={item.teamAName}/>
+                        </span>
+                        <span className="indiaImageContainer">
+                            <img src={teamBSmallImage} alt={item.teamBName}/>
+                        </span>
                     </div>
 
                     <span className="result-container col-md-4 col-sm-12 col-lg-4">{item.result}</span>
@@ -32,20 +32,20 @@ export default class MatchesList extends Component {
                     <Collapsible trigger="Schedule & Scores" className="">
                         <div className={`view-more`} >
                             <p className="synopsis-container">
-                            	<div className="teamAScoreContainer col-sm-4">
-                            		<span className="teamAName">{item.teamAName}</span>
-                            		<span className="teamAImageContainer">
-                            			<img src={teamALargeImage} alt={item.teamAName}/>
-                            		</span>
-                            	</div>
-                            	<span className="col-sm-4">vs</span>
-                            	<div className="teamBScoreContainer col-sm-4">
-                            		<span className="teamBName">{item.teamBName}</span>
-                            		<span className="teamBImageContainer">
-                            			<img src={teamBLargeImage} alt={item.teamBName}/>
-                            		</span>
-                            	</div>
-                            	<span className="result">{item.result}</span>
+                                <div className="teamAScoreContainer col-sm-2 col-md-4">
+                                    <span className="teamAName">{item.teamAName}</span>
+                                    <span className="teamAImageContainer">
+                                        <img src={teamALargeImage} alt={item.teamAName}/>
+                                    </span>
+                                </div>
+                                <span className="col-sm-2 col-md-4">vs</span>
+                                <div className="teamBScoreContainer col-sm-2 col-md-4">
+                                    <span className="teamBName">{item.teamBName}</span>
+                                    <span className="teamBImageContainer">
+                                        <img src={teamBLargeImage} alt={item.teamBName}/>
+                                    </span>
+                                </div>
+                                <span className="result">{item.result}</span>
                             </p>
                         </div>
                     </Collapsible>
